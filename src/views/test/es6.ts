0cf23dfd29c3f92e008251bf7d80081935e23929
hlliu2 <762366705@qq.com>
2023-07-28 10:55:37 +0800

    # 更新

---
+++ src/views/test/es6.ts
@@ -0,0 +1,4 @@
+export let aa = 12
+setTimeout(() => {
+	console.log(aa)
+}, 5000)
--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -5,17 +5,30 @@
 		<inp></inp>
 		<test3></test3>
 		{{ num }}
-        <div>
-            <el-button @click="add">add</el-button>
-		<el-button @click="changeName">changeName</el-button>
-        </div>
-		
+		<div>
+			<el-button @click="add">add</el-button>
+			<el-button @click="changeName">changeName</el-button>
+		</div>
+		<div>
+			<el-button @click="changeAA">测试es6 导入的变量是否能够修改</el-button>
+		</div>
 	</section>
 </template>
 <script lang="ts" setup>
 import inp from './inp.vue'
 import Hub from './hub'
 import test3 from '../daily/test-vue3.vue'
+import { aa } from './es6'
+function changeAA() {
+	console.log(aa)
+	// console.log(test3.render())
+	// Cannot assign to 'aa' because it is an import.
+	// aa += 1
+	// es6的导入类似const只读的，但是能修改其引用空间数据，所以如果aa是对象，数据就可以改变内部值
+}
+// test3.then((res: any) => {
+// 	console.log(res)
+// })
 let name = ref('')
 Hub.on('sendName', (val: any) => {
 	name.value = `Hello, ${val}`
@@ -37,10 +50,10 @@ watchEffect(onInvalidate => {
 	// const timer = setInterval(() => {
 	// 	console.log('发请网络请求')
 	// }, 1000)
-    // 打印num value 1秒后才打印发请网络请求，并且会清除timer
+	// 打印num value 1秒后才打印发请网络请求，并且会清除timer
 	onInvalidate(() => {
-        // 这里默认进来不执行，只有手动点击按钮才执行
-        console.log('hello')
+		// 这里默认进来不执行，只有手动点击按钮才执行
+		console.log('hello')
 		// clearInterval(timer)
 	})
 	console.log(num.value)
